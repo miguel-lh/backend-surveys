273cb8480a9d5f3219c6e0aa5548a4b8e1e2072b
Miguel <miguel.isel17@gmail.com>
2024-12-11 11:40:39 -0600

    fix cors_allowd_origins

--- config/settings/production.py
+++ config/settings/production.py
@@ -11,7 +11,10 @@ from .base import env
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["*"])
+ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["*",])
+
+CORS_ALLOWED_ORIGINS = ["*",]
+print(ALLOWED_HOSTS)
 
 # DATABASES
 # ------------------------------------------------------------------------------
